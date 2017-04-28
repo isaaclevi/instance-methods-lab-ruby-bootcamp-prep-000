03508a652f8d37c76d097ea8f049b82d5cb320b3
isaac LEVI <isaaclevi1710@gmail.com>
2017-04-28 03:25:57 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,11 @@
+#learn open instance-methods-lab-ruby-bootcamp-prep-000
+class Dog
+  def bark
+    puts "Woof!"
+  end
+
+  def sit
+    puts "The Dog is sitting"
+  end
+  
+end
---
+++ lib/person.rb
@@ -0,0 +1,11 @@
+#learn open instance-methods-lab-ruby-bootcamp-prep-000
+class Person
+  def talk
+    puts "Hello World!"
+  end
+
+  def walk
+    puts "The Person is walking"
+  end
+  
+end
